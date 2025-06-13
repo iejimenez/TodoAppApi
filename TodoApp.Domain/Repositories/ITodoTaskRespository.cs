a9804c20de2d9fc2940b108f8ac95260191cc411
Ivan.Jimenez <iejimenez01@gmail.com>
2025-06-12 22:03:34 -0500

    Creamos el repositorio de las tareas con las funciones basicas del crud que se van a implementar

---
+++ TodoApp.Domain/Repositories/ITodoTaskRespository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Domain.Repositories
+{
+    public interface ITodoTaskRespository
+    {
+        Task<TodoTask> GetByIdAsync(Guid id);
+        Task<IEnumerable<TodoTask>> GetAllAsync();
+        Task AddAsync(TodoTask todo);
+        Task UpdateAsync(TodoTask todo);
+        Task DeleteAsync(Guid id);
+    }
+}
