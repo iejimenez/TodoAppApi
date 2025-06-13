2d566f294cff44d19466f036852b6e6cd87d88c6
Ivan.Jimenez <iejimenez01@gmail.com>
2025-06-12 21:55:53 -0500

    Creamos la entidad para las tareas

---
+++ TodoApp.Domain/Entities/TodoTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Domain.Entities
+{
+    public class TodoTask
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? CompletedAt { get; private set; }
+
+        private TodoTask() { } // For EF Core
+
+        public TodoTask(string title, string description)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
