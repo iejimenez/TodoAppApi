c127ee123bd8912db1a394e78682eeb9e5aac82d
Ivan.Jimenez <iejimenez01@gmail.com>
2025-06-12 22:48:35 -0500

    Ahora si implementamos el servicio pero solo las dos funcionalidades de consulta por id y todos mas un par de mappers para lista y para un solo item

--- TodoApp.Application/Services/TodoTaskService.cs
+++ TodoApp.Application/Services/TodoTaskService.cs
@@ -5,19 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 using TodoApp.Application.DTOs;
 using TodoApp.Application.Interfaces;
+using TodoApp.Domain.Entities;
+using TodoApp.Domain.Repositories;
 
 namespace TodoApp.Application.Services
 {
     public class TodoTaskService : ITodoTaskService
     {
-        public Task<IEnumerable<TodoTaskDto>> GetAllAsync()
+        private readonly ITodoTaskRespository _todoTaskRepository;
+        public TodoTaskService(ITodoTaskRespository todoTaskRepository)
         {
-            throw new NotImplementedException();
+            _todoTaskRepository = todoTaskRepository;
+        }
+        public async Task<IEnumerable<TodoTaskDto>> GetAllAsync()
+        {
+            var todoTask = await _todoTaskRepository.GetAllAsync();
+            return MapToDtoList(todoTask);
         }
 
-        public Task<TodoTaskDto> GetByIdAsync(Guid id)
+        public async Task<TodoTaskDto> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var todoTask = await _todoTaskRepository.GetByIdAsync(id);
+            return MapToDto(todoTask);
+        }
+
+        private static TodoTaskDto MapToDto(TodoTask todo)
+        {
+            return new TodoTaskDto
+            {
+                Id = todo.Id,
+                Title = todo.Title,
+                Description = todo.Description,
+                IsCompleted = todo.IsCompleted,
+                CreatedAt = todo.CreatedAt,
+                CompletedAt = todo.CompletedAt
+            };
+        }
+
+        private static IEnumerable<TodoTaskDto> MapToDtoList(IEnumerable<TodoTask> todoTasks)
+        {
+            var dtoList = new List<TodoTaskDto>();
+            foreach (var todoTask in todoTasks)
+            {
+                dtoList.Add(MapToDto(todoTask));
+            }
+            return dtoList;
         }
     }
 }
