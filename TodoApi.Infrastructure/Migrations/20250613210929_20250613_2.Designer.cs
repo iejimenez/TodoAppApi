843694aa4779a671334880342738927f0893a3be
Ivan Jimenez <Ivan.Jimenez@fisegroup.com>
2025-06-13 16:12:53 -0500

    Correccion en error en la entidades al migrar la bd, se crea migracion para actualizar el modelo en la bd

--- TodoApi.Infrastructure/Migrations/20250613210929_20250613_2.Designer.cs
+++ TodoApi.Infrastructure/Migrations/20250613210929_20250613_2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TodoApi.Infrastructure.Data;
@@ -11,9 +12,11 @@ using TodoApi.Infrastructure.Data;
 namespace TodoApi.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250613210929_20250613_2")]
+    partial class _20250613_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
