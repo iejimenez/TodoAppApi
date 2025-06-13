12d0c76da71ff506febe813b78b79b1df0ebea0c
Ivan.Jimenez <iejimenez01@gmail.com>
2025-06-12 23:14:01 -0500

    Cree el controllador con la implementacion para los dos metodos del servicio, y añadi swagger  al api

---
+++ TodoApp.Api/Controllers/TodoTaskController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Application.DTOs;
+using TodoApp.Application.Interfaces;
+
+namespace TodoApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoTaskController : ControllerBase
+    {
+        private readonly ITodoTaskService _todoTaskService;
+
+        public TodoTaskController(ITodoTaskService todoTaskService)
+        {
+            _todoTaskService = todoTaskService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoTaskDto>>> GetAll()
+        {
+            var todos = await _todoTaskService.GetAllAsync();
+            return Ok(todos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoTaskDto>> GetById(Guid id)
+        {
+            try
+            {
+                var todo = await _todoTaskService.GetByIdAsync(id);
+                return Ok(todo);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- TodoApp.Api/Program.cs
+++ TodoApp.Api/Program.cs
@@ -11,7 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Add DbContext
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -25,6 +28,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
